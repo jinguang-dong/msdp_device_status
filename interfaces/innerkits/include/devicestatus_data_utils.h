8c806735a484e2f8b03f05837fd7ec0c68dce05b
zhao-y-zhao <zhao-y-zhao@neusoft.com>
2022-09-28 14:45:51 +0800

    call DevicestatusAlgorithmManagerInterface

--- interfaces/innerkits/include/devicestatus_data_utils.h
+++ interfaces/innerkits/include/devicestatus_data_utils.h
@@ -29,17 +29,47 @@ public:
         TYPE_LID_OPEN
     };
 
+    enum DevicestatusTypeValue {
+        INVALID = 0,
+        VALID,
+    };
+
     enum DevicestatusValue {
         VALUE_INVALID = -1,
         VALUE_ENTER,
         VALUE_EXIT
     };
 
+    enum DevicestatusActivityEvent {
+        EVENT_INVALID = 0,
+        ENTER = 1,
+        EXIT = 2,
+        ENTER_EXIT = 3
+    };
+    enum DevicestatusReportLatencyNs {
+        Latency_INVALID = -1,
+    };
+    enum Status {
+        STATUS_INVALID = -1,
+    };
+    enum Action {
+        ACTION_INVALID = -1,
+        ACTION_ENLARGE,
+        ACTION_REDUCE,
+        ACTION_UP,
+        ACTION_LEFT,
+        ACTION_DOWN,
+        ACTION_RIGHT
+    };
     struct DevicestatusData {
         DevicestatusType type;
         DevicestatusValue value;
+        Status status;
+        Action action;
+        double move;
     };
 };
+	static int32_t in_vector_count[DevicestatusDataUtils::DevicestatusType::TYPE_LID_OPEN + 1] = {0};
 } // namespace Msdp
 } // namespace OHOS
 #endif // DEVICESTATUS_DATA_UTILS_H
