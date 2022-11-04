baa987d43570c8c703294110ff2f43e372a138e0
hhh2 <pengfei.hou@huawei.com>
2022-11-04 11:49:27 +0800

    Modify bytrace_adapter,devicestatus_agent,devicestatus_callback_proxy,devicestatus_client,devicestatus_srv_proxy Signed-off-by:hhh2<pengfei.hou@huawei.com>
    Signed-off-by: hhh2 <pengfei.hou@huawei.com>

--- frameworks/native/src/devicestatus_client.cpp
+++ frameworks/native/src/devicestatus_client.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,21 +13,21 @@
  * limitations under the License.
  */
 
-#include "devicestatus_client.h"
-
 #include <if_system_ability_manager.h>
 #include <iservice_registry.h>
 #include <system_ability_definition.h>
 
-#include "devicestatus_common.h"
-
 #include "iremote_broker.h"
 #include "iremote_object.h"
 
+#include "devicestatus_client.h"
+#include "devicestatus_common.h"
+
 namespace OHOS {
 namespace Msdp {
-DevicestatusClient::DevicestatusClient() {}
-DevicestatusClient::~DevicestatusClient()
+namespace DeviceStatus {
+DeviceStatusClient::DeviceStatusClient() {}
+DeviceStatusClient::~DeviceStatusClient()
 {
     if (devicestatusProxy_ != nullptr) {
         auto remoteObject = devicestatusProxy_->AsObject();
@@ -37,7 +37,7 @@ DevicestatusClient::~DevicestatusClient()
     }
 }
 
-ErrCode DevicestatusClient::Connect()
+ErrCode DeviceStatusClient::Connect()
 {
     std::lock_guard<std::mutex> lock(mutex_);
     if (devicestatusProxy_ != nullptr) {
@@ -45,38 +45,40 @@ ErrCode DevicestatusClient::Connect()
         return ERR_OK;
     }
 
-    sptr<ISystemAbilityManager> sam = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
-    if (sam == nullptr) {
+    sptr<ISystemAbilityManager> sa = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+    if (sa == nullptr) {
         DEV_HILOGE(INNERKIT, "GetSystemAbilityManager failed");
         return E_DEVICESTATUS_GET_SYSTEM_ABILITY_MANAGER_FAILED;
     }
 
-    sptr<IRemoteObject> remoteObject_ = sam->CheckSystemAbility(MSDP_DEVICESTATUS_SERVICE_ID);
-    if (remoteObject_ == nullptr) {
+    sptr<IRemoteObject> remoteObject = sa->CheckSystemAbility(MSDP_DEVICESTATUS_SERVICE_ID);
+    if (remoteObject == nullptr) {
         DEV_HILOGE(INNERKIT, "CheckSystemAbility failed");
         return E_DEVICESTATUS_GET_SERVICE_FAILED;
     }
 
-    deathRecipient_ = sptr<IRemoteObject::DeathRecipient>(new DevicestatusDeathRecipient());
+    deathRecipient_ = sptr<IRemoteObject::DeathRecipient>(new (std::nothrow) DeviceStatusDeathRecipient());
     if (deathRecipient_ == nullptr) {
-        DEV_HILOGE(INNERKIT, "Failed to create DevicestatusDeathRecipient");
+        DEV_HILOGE(INNERKIT, "Failed to create DeviceStatusDeathRecipient");
         return ERR_NO_MEMORY;
     }
 
-    if ((remoteObject_->IsProxyObject()) && (!remoteObject_->AddDeathRecipient(deathRecipient_))) {
-        DEV_HILOGE(INNERKIT, "Add death recipient to Devicestatus service failed");
-        return E_DEVICESTATUS_ADD_DEATH_RECIPIENT_FAILED;
+    if (remoteObject->IsProxyObject()) {
+        if (!remoteObject->AddDeathRecipient(deathRecipient_)) {
+            DEV_HILOGE(INNERKIT, "Add death recipient to DeviceStatus service failed");
+            return E_DEVICESTATUS_ADD_DEATH_RECIPIENT_FAILED;
+        }
     }
 
-    devicestatusProxy_ = iface_cast<Idevicestatus>(remoteObject_);
-    DEV_HILOGD(INNERKIT, "Connecting DevicestatusService success");
+    devicestatusProxy_ = iface_cast<Idevicestatus>(remoteObject);
+    DEV_HILOGD(INNERKIT, "Connecting DeviceStatusService success");
     return ERR_OK;
 }
 
-void DevicestatusClient::ResetProxy(const wptr<IRemoteObject>& remote)
+void DeviceStatusClient::ResetProxy(const wptr<IRemoteObject>& remote)
 {
     std::lock_guard<std::mutex> lock(mutex_);
-    DEVICESTATUS_RETURN_IF(devicestatusProxy_ == nullptr);
+    DEV_RET_IF_NULL(devicestatusProxy_ == nullptr);
 
     auto serviceRemote = devicestatusProxy_->AsObject();
     if ((serviceRemote != nullptr) && (serviceRemote == remote.promote())) {
@@ -85,70 +87,78 @@ void DevicestatusClient::ResetProxy(const wptr<IRemoteObject>& remote)
     }
 }
 
-void DevicestatusClient::DevicestatusDeathRecipient::OnRemoteDied(const wptr<IRemoteObject>& remote)
+void DeviceStatusClient::DeviceStatusDeathRecipient::OnRemoteDied(const wptr<IRemoteObject>& remote)
 {
     if (remote == nullptr) {
         DEV_HILOGE(INNERKIT, "OnRemoteDied failed, remote is nullptr");
         return;
     }
 
-    DevicestatusClient::GetInstance().ResetProxy(remote);
+    DeviceStatusClient::GetInstance().ResetProxy(remote);
     DEV_HILOGD(INNERKIT, "Recv death notice");
 }
 
-void DevicestatusClient::SubscribeCallback(const DevicestatusDataUtils::DevicestatusType& type, \
-    const sptr<IdevicestatusCallback>& callback)
+void DeviceStatusClient::SubscribeCallback(Type type,
+    ActivityEvent event,
+    ReportLatencyNs latency,
+    sptr<IRemoteDevStaCallbck> callback)
 {
-    DEV_HILOGD(INNERKIT, "Enter");
-    DEVICESTATUS_RETURN_IF((callback == nullptr) || (Connect() != ERR_OK));
+    DEV_HILOGI(INNERKIT, "Enter event:%{public}d,latency:%{public}d", event, latency);
+    typeMap_.insert(std::make_pair(type,1));
+    DEV_HILOGD(INNERKIT, "typeMap_ %{public}d, type: %{public}d", typeMap_[type], type);
+    DEV_RET_IF_NULL((callback == nullptr) || (Connect() != ERR_OK));
     if (devicestatusProxy_ == nullptr) {
         DEV_HILOGE(SERVICE, "devicestatusProxy_ is nullptr");
         return;
     }
-    if (type > DevicestatusDataUtils::DevicestatusType::TYPE_INVALID
-        && type <= DevicestatusDataUtils::DevicestatusType::TYPE_LID_OPEN) {
-        devicestatusProxy_->Subscribe(type, callback);
+    if (type > Type::TYPE_INVALID
+        && type <= Type::TYPE_LID_OPEN) {
+        devicestatusProxy_->Subscribe(type, event, latency, callback);
+    }
+    return;
+}
+
+void DeviceStatusClient::UnSubscribeCallback(Type type,
+    ActivityEvent event,
+    sptr<IRemoteDevStaCallbck> callback)
+{
+    DEV_HILOGI(INNERKIT, "UNevent: %{public}d", event);
+    typeMap_.erase(type);
+    DEV_HILOGD(INNERKIT, "typeMap_ %{public}d", typeMap_[type]);
+    DEV_RET_IF_NULL((callback == nullptr) || (Connect() != ERR_OK));
+    if (devicestatusProxy_ == nullptr) {
+        DEV_HILOGE(SERVICE, "devicestatusProxy_ is nullptr");
+        return;
+    }
+    if (type > Type::TYPE_INVALID && type <= Type::TYPE_LID_OPEN) {
+        devicestatusProxy_->UnSubscribe(type, event, callback);
+    }
+    if (event > ActivityEvent::EVENT_INVALID && event <= ActivityEvent::ENTER_EXIT) {
+        DEV_HILOGE(SERVICE, "event is failed");
     }
     return;
     DEV_HILOGD(INNERKIT, "Exit");
 }
 
-void DevicestatusClient::UnSubscribeCallback(const DevicestatusDataUtils::DevicestatusType& type, \
-    const sptr<IdevicestatusCallback>& callback)
+Data DeviceStatusClient::GetDeviceStatusData(Type type)
 {
     DEV_HILOGD(INNERKIT, "Enter");
-    DEVICESTATUS_RETURN_IF((callback == nullptr) || (Connect() != ERR_OK));
-    if (devicestatusProxy_ == nullptr) {
-        DEV_HILOGE(SERVICE, "devicestatusProxy_ is nullptr");
-        return;
-    }
-    if (type > DevicestatusDataUtils::DevicestatusType::TYPE_INVALID
-        && type <= DevicestatusDataUtils::DevicestatusType::TYPE_LID_OPEN) {
-        devicestatusProxy_->UnSubscribe(type, callback);
-    }
-    return;
-    DEV_HILOGD(INNERKIT, "Exit");
-}
+    Data devicestatusData;
+    devicestatusData.type = Type::TYPE_INVALID;
+    devicestatusData.value = OnChangedValue::VALUE_INVALID;
 
-DevicestatusDataUtils::DevicestatusData DevicestatusClient::GetDevicestatusData(const \
-    DevicestatusDataUtils::DevicestatusType& type)
-{
-    DEV_HILOGD(INNERKIT, "Enter");
-    DevicestatusDataUtils::DevicestatusData devicestatusData;
-    devicestatusData.type = DevicestatusDataUtils::DevicestatusType::TYPE_INVALID;
-    devicestatusData.value = DevicestatusDataUtils::DevicestatusValue::VALUE_INVALID;
-
-    DEVICESTATUS_RETURN_IF_WITH_RET((Connect() != ERR_OK), devicestatusData);
+    DEV_RET_IF_NULL_WITH_RET((Connect() != ERR_OK), devicestatusData);
     if (devicestatusProxy_ == nullptr) {
         DEV_HILOGE(SERVICE, "devicestatusProxy_ is nullptr");
         return devicestatusData;
     }
-    if (type > DevicestatusDataUtils::DevicestatusType::TYPE_INVALID
-        && type <= DevicestatusDataUtils::DevicestatusType::TYPE_LID_OPEN) {
+    if (type > Type::TYPE_INVALID
+        && type <= Type::TYPE_LID_OPEN) {
         devicestatusData = devicestatusProxy_->GetCache(type);
     }
     DEV_HILOGD(INNERKIT, "Exit");
     return devicestatusData;
 }
+} // namespace DeviceStatus
 } // namespace Msdp
 } // namespace OHOS
