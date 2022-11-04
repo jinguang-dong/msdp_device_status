24df71ac520f25021bd8e4256e9d5c4db7a22b9b
hhh2 <pengfei.hou@huawei.com>
2022-11-04 17:08:17 +0800

    Modify devicestatus_callback_stub and devicestatus_event Signed-off-by:hhh2<pengfei.hou@huawei.com>
    Signed-off-by: hhh2 <pengfei.hou@huawei.com>

---
+++ frameworks/js/napi/include/device_status_napi.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DEVICESTATUS_NAPI_H
+#define DEVICESTATUS_NAPI_H
+
+#include <map>
+
+#include "napi/native_api.h"
+#include "napi/native_node_api.h"
+#include "device_status_callback_stub.h"
+#include "devicestatus_data_utils.h"
+#include "device_status_event.h"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+class DeviceStatusCallback : public DeviceStatusCallbackStub {
+public:
+    explicit DeviceStatusCallback(napi_env env) : env_(env) {}
+    virtual ~DeviceStatusCallback() {};
+    void OnDeviceStatusChanged(const Data& devicestatusData) override;
+    static void EmitOnEvent(uv_work_t *work, int status);
+private:
+    // Data data_ {};
+    napi_env env_ = nullptr;
+};
+
+class DeviceStatusNapi : public DeviceStatusEvent {
+public:
+    explicit DeviceStatusNapi(napi_env env, napi_value thisVar);
+    virtual ~DeviceStatusNapi();
+
+    static napi_value Init(napi_env env, napi_value exports);
+    static napi_value SubscribeDeviceStatus(napi_env env, napi_callback_info info);
+    static napi_value UnSubscribeDeviceStatus(napi_env env, napi_callback_info info);
+    static napi_value GetDeviceStatus(napi_env env, napi_callback_info info);
+    static bool CheckArguments(napi_env env, napi_callback_info info);
+    static bool CheckUnsubArguments(napi_env env, napi_callback_info info);
+    static bool CheckGetArguments(napi_env env, napi_callback_info info);
+
+    static int32_t ConvertTypeToInt(const std::string &type);
+    void OnDeviceStatusChangedDone(const int32_t& type, const int32_t& value, bool isOnce);
+    static DeviceStatusNapi* GetDeviceStatusNapi();
+    static std::map<int32_t, sptr<IRemoteDevStaCallbck>> callbackMap_;
+
+private:
+    napi_ref callbackRef_;
+    static napi_ref devicestatusValueRef_;
+    napi_env env_;
+};
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
+#endif // DEVICESTATUS_NAPI_H
