cc61f9e7c11022881bd4061621e6395409708158
wuliangdong <wuliangdong1@huawei.com>
2023-02-23 07:41:24 +0000

    something wrong
    Signed-off-by: wuliangdong <wuliangdong1@huawei.com>
    Change-Id: I8cfd66a2133296735de96b090e2ddbff6cf5d7aa

--- frameworks/native/interaction/test/interaction_manager_test.cpp
+++ frameworks/native/interaction/test/interaction_manager_test.cpp
@@ -96,158 +96,158 @@ int32_t SetParam(int32_t width, int32_t height, DragData& dragData)
     return RET_OK;
 }
 
-/**
- * @tc.name: InteractionManagerTest_RegisterCoordinationListener_001
- * @tc.desc: Register coordination listener
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InteractionManagerTest, InteractionManagerTest_RegisterCoordinationListener_001, TestSize.Level1)
-{
-    CALL_TEST_DEBUG;
-    std::shared_ptr<ICoordinationListener> consumer = nullptr;
-    int32_t ret = InteractionManager::GetInstance()->RegisterCoordinationListener(consumer);
-#ifdef OHOS_BUILD_ENABLE_COORDINATION
-    ASSERT_EQ(ret, RET_ERR);
-#else
-    ASSERT_EQ(ret, ERROR_UNSUPPORT);
-#endif // OHOS_BUILD_ENABLE_COORDINATION
-}
+// /**
+//  * @tc.name: InteractionManagerTest_RegisterCoordinationListener_001
+//  * @tc.desc: Register coordination listener
+//  * @tc.type: FUNC
+//  * @tc.require:
+//  */
+// HWTEST_F(InteractionManagerTest, InteractionManagerTest_RegisterCoordinationListener_001, TestSize.Level1)
+// {
+//     CALL_TEST_DEBUG;
+//     std::shared_ptr<ICoordinationListener> consumer = nullptr;
+//     int32_t ret = InteractionManager::GetInstance()->RegisterCoordinationListener(consumer);
+// #ifdef OHOS_BUILD_ENABLE_COORDINATION
+//     ASSERT_EQ(ret, RET_ERR);
+// #else
+//     ASSERT_EQ(ret, ERROR_UNSUPPORT);
+// #endif // OHOS_BUILD_ENABLE_COORDINATION
+// }
 
-/**
- * @tc.name: InteractionManagerTest_RegisterCoordinationListener_002
- * @tc.desc: Register coordination listener
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InteractionManagerTest, InteractionManagerTest_RegisterCoordinationListener_002, TestSize.Level1)
-{
-    CALL_DEBUG_ENTER;
-    class CoordinationListenerTest : public ICoordinationListener {
-    public:
-        CoordinationListenerTest() : ICoordinationListener() {}
-        void OnCoordinationMessage(const std::string &deviceId, CoordinationMessage msg) override
-        {
-            FI_HILOGD("RegisterCoordinationListenerTest");
-        };
-    };
-    std::shared_ptr<CoordinationListenerTest> consumer =
-        std::make_shared<CoordinationListenerTest>();
-    int32_t ret = InteractionManager::GetInstance()->RegisterCoordinationListener(consumer);
-#ifdef OHOS_BUILD_ENABLE_COORDINATION
-    ASSERT_EQ(ret, RET_OK);
-#else
-    ASSERT_EQ(ret, ERROR_UNSUPPORT);
-#endif // OHOS_BUILD_ENABLE_COORDINATION
-    ret = InteractionManager::GetInstance()->UnregisterCoordinationListener(consumer);
-#ifdef OHOS_BUILD_ENABLE_COORDINATION
-    ASSERT_EQ(ret, RET_OK);
-#else
-    ASSERT_EQ(ret, ERROR_UNSUPPORT);
-#endif // OHOS_BUILD_ENABLE_COORDINATION
-}
+// /**
+//  * @tc.name: InteractionManagerTest_RegisterCoordinationListener_002
+//  * @tc.desc: Register coordination listener
+//  * @tc.type: FUNC
+//  * @tc.require:
+//  */
+// HWTEST_F(InteractionManagerTest, InteractionManagerTest_RegisterCoordinationListener_002, TestSize.Level1)
+// {
+//     CALL_DEBUG_ENTER;
+//     class CoordinationListenerTest : public ICoordinationListener {
+//     public:
+//         CoordinationListenerTest() : ICoordinationListener() {}
+//         void OnCoordinationMessage(const std::string &deviceId, CoordinationMessage msg) override
+//         {
+//             FI_HILOGD("RegisterCoordinationListenerTest");
+//         };
+//     };
+//     std::shared_ptr<CoordinationListenerTest> consumer =
+//         std::make_shared<CoordinationListenerTest>();
+//     int32_t ret = InteractionManager::GetInstance()->RegisterCoordinationListener(consumer);
+// #ifdef OHOS_BUILD_ENABLE_COORDINATION
+//     ASSERT_EQ(ret, RET_OK);
+// #else
+//     ASSERT_EQ(ret, ERROR_UNSUPPORT);
+// #endif // OHOS_BUILD_ENABLE_COORDINATION
+//     ret = InteractionManager::GetInstance()->UnregisterCoordinationListener(consumer);
+// #ifdef OHOS_BUILD_ENABLE_COORDINATION
+//     ASSERT_EQ(ret, RET_OK);
+// #else
+//     ASSERT_EQ(ret, ERROR_UNSUPPORT);
+// #endif // OHOS_BUILD_ENABLE_COORDINATION
+// }
 
-/**
- * @tc.name: InteractionManagerTest_UnregisterCoordinationListener
- * @tc.desc: Unregister coordination listener
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InteractionManagerTest, InteractionManagerTest_UnregisterCoordinationListener, TestSize.Level1)
-{
-    CALL_TEST_DEBUG;
-    std::shared_ptr<ICoordinationListener> consumer = nullptr;
-    int32_t ret = InteractionManager::GetInstance()->UnregisterCoordinationListener(consumer);
-#ifdef OHOS_BUILD_ENABLE_COORDINATION
-    ASSERT_EQ(ret, RET_OK);
-#else
-    ASSERT_EQ(ret, ERROR_UNSUPPORT);
-#endif // OHOS_BUILD_ENABLE_COORDINATION
-}
+// /**
+//  * @tc.name: InteractionManagerTest_UnregisterCoordinationListener
+//  * @tc.desc: Unregister coordination listener
+//  * @tc.type: FUNC
+//  * @tc.require:
+//  */
+// HWTEST_F(InteractionManagerTest, InteractionManagerTest_UnregisterCoordinationListener, TestSize.Level1)
+// {
+//     CALL_TEST_DEBUG;
+//     std::shared_ptr<ICoordinationListener> consumer = nullptr;
+//     int32_t ret = InteractionManager::GetInstance()->UnregisterCoordinationListener(consumer);
+// #ifdef OHOS_BUILD_ENABLE_COORDINATION
+//     ASSERT_EQ(ret, RET_OK);
+// #else
+//     ASSERT_EQ(ret, ERROR_UNSUPPORT);
+// #endif // OHOS_BUILD_ENABLE_COORDINATION
+// }
 
-/**
- * @tc.name: InteractionManagerTest_EnableCoordination
- * @tc.desc: Enable coordination
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InteractionManagerTest, InteractionManagerTest_EnableCoordination, TestSize.Level1)
-{
-    CALL_TEST_DEBUG;
-    bool enabled = false;
-    auto fun = [](std::string listener, CoordinationMessage coordinationMessages) {
-        FI_HILOGD("Enable coordination success");
-    };
-    int32_t ret = InteractionManager::GetInstance()->EnableCoordination(enabled, fun);
-#ifdef OHOS_BUILD_ENABLE_COORDINATION
-    ASSERT_EQ(ret, RET_OK);
-#else
-    ASSERT_EQ(ret, ERROR_UNSUPPORT);
-#endif // OHOS_BUILD_ENABLE_COORDINATION
-}
+// /**
+//  * @tc.name: InteractionManagerTest_EnableCoordination
+//  * @tc.desc: Enable coordination
+//  * @tc.type: FUNC
+//  * @tc.require:
+//  */
+// HWTEST_F(InteractionManagerTest, InteractionManagerTest_EnableCoordination, TestSize.Level1)
+// {
+//     CALL_TEST_DEBUG;
+//     bool enabled = false;
+//     auto fun = [](std::string listener, CoordinationMessage coordinationMessages) {
+//         FI_HILOGD("Enable coordination success");
+//     };
+//     int32_t ret = InteractionManager::GetInstance()->EnableCoordination(enabled, fun);
+// #ifdef OHOS_BUILD_ENABLE_COORDINATION
+//     ASSERT_EQ(ret, RET_OK);
+// #else
+//     ASSERT_EQ(ret, ERROR_UNSUPPORT);
+// #endif // OHOS_BUILD_ENABLE_COORDINATION
+// }
 
-/**
- * @tc.name: InteractionManagerTest_StartCoordination
- * @tc.desc: Start coordination
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InteractionManagerTest, InteractionManagerTest_StartCoordination, TestSize.Level1)
-{
-    CALL_TEST_DEBUG;
-    std::string sinkDeviceId("");
-    int32_t srcDeviceId = -1;
-    auto fun = [](std::string listener, CoordinationMessage coordinationMessages) {
-        FI_HILOGD("Start coordination success");
-    };
-    int32_t ret = InteractionManager::GetInstance()->StartCoordination(sinkDeviceId, srcDeviceId, fun);
-#ifdef OHOS_BUILD_ENABLE_COORDINATION
-    ASSERT_NE(ret, RET_OK);
-#else
-    ASSERT_EQ(ret, ERROR_UNSUPPORT);
-#endif // OHOS_BUILD_ENABLE_COORDINATION
-}
+// /**
+//  * @tc.name: InteractionManagerTest_StartCoordination
+//  * @tc.desc: Start coordination
+//  * @tc.type: FUNC
+//  * @tc.require:
+//  */
+// HWTEST_F(InteractionManagerTest, InteractionManagerTest_StartCoordination, TestSize.Level1)
+// {
+//     CALL_TEST_DEBUG;
+//     std::string sinkDeviceId("");
+//     int32_t srcDeviceId = -1;
+//     auto fun = [](std::string listener, CoordinationMessage coordinationMessages) {
+//         FI_HILOGD("Start coordination success");
+//     };
+//     int32_t ret = InteractionManager::GetInstance()->StartCoordination(sinkDeviceId, srcDeviceId, fun);
+// #ifdef OHOS_BUILD_ENABLE_COORDINATION
+//     ASSERT_NE(ret, RET_OK);
+// #else
+//     ASSERT_EQ(ret, ERROR_UNSUPPORT);
+// #endif // OHOS_BUILD_ENABLE_COORDINATION
+// }
 
-/**
- * @tc.name: InteractionManagerTest_StopCoordination
- * @tc.desc: Stop coordination
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InteractionManagerTest, InteractionManagerTest_StopCoordination, TestSize.Level1)
-{
-    CALL_TEST_DEBUG;
-    auto fun = [](std::string listener, CoordinationMessage coordinationMessages) {
-        FI_HILOGD("Stop coordination success");
-    };
-    int32_t ret = InteractionManager::GetInstance()->StopCoordination(fun);
-#ifdef OHOS_BUILD_ENABLE_COORDINATION
-    ASSERT_NE(ret, ERROR_UNSUPPORT);
-#else
-    ASSERT_EQ(ret, ERROR_UNSUPPORT);
-#endif // OHOS_BUILD_ENABLE_COORDINATION
-}
+// /**
+//  * @tc.name: InteractionManagerTest_StopCoordination
+//  * @tc.desc: Stop coordination
+//  * @tc.type: FUNC
+//  * @tc.require:
+//  */
+// HWTEST_F(InteractionManagerTest, InteractionManagerTest_StopCoordination, TestSize.Level1)
+// {
+//     CALL_TEST_DEBUG;
+//     auto fun = [](std::string listener, CoordinationMessage coordinationMessages) {
+//         FI_HILOGD("Stop coordination success");
+//     };
+//     int32_t ret = InteractionManager::GetInstance()->StopCoordination(fun);
+// #ifdef OHOS_BUILD_ENABLE_COORDINATION
+//     ASSERT_NE(ret, ERROR_UNSUPPORT);
+// #else
+//     ASSERT_EQ(ret, ERROR_UNSUPPORT);
+// #endif // OHOS_BUILD_ENABLE_COORDINATION
+// }
 
-/**
- * @tc.name: InteractionManagerTest_GetCoordinationState
- * @tc.desc: Get coordination state
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InteractionManagerTest, InteractionManagerTest_GetCoordinationState, TestSize.Level1)
-{
-    CALL_TEST_DEBUG;
-    const std::string deviceId("");
-    auto fun = [](bool state) {
-        FI_HILOGD("Get coordination state success");
-    };
-    int32_t ret = InteractionManager::GetInstance()->GetCoordinationState(deviceId, fun);
-#ifdef OHOS_BUILD_ENABLE_COORDINATION
-    ASSERT_EQ(ret, RET_OK);
-#else
-    ASSERT_EQ(ret, ERROR_UNSUPPORT);
-#endif // OHOS_BUILD_ENABLE_COORDINATION
-}
+// /**
+//  * @tc.name: InteractionManagerTest_GetCoordinationState
+//  * @tc.desc: Get coordination state
+//  * @tc.type: FUNC
+//  * @tc.require:
+//  */
+// HWTEST_F(InteractionManagerTest, InteractionManagerTest_GetCoordinationState, TestSize.Level1)
+// {
+//     CALL_TEST_DEBUG;
+//     const std::string deviceId("");
+//     auto fun = [](bool state) {
+//         FI_HILOGD("Get coordination state success");
+//     };
+//     int32_t ret = InteractionManager::GetInstance()->GetCoordinationState(deviceId, fun);
+// #ifdef OHOS_BUILD_ENABLE_COORDINATION
+//     ASSERT_EQ(ret, RET_OK);
+// #else
+//     ASSERT_EQ(ret, ERROR_UNSUPPORT);
+// #endif // OHOS_BUILD_ENABLE_COORDINATION
+// }
 
 /**
  * @tc.name: InteractionManagerTest_StartDrag
@@ -282,18 +282,18 @@ HWTEST_F(InteractionManagerTest, InteractionManagerTest_StopDrag, TestSize.Level
     ASSERT_EQ(ret, RET_OK);
 }
 
- /**
-*  @tc.name: InteractionManagerTest_GetDragTargetPid
- * @tc.desc: Get Drag Target Pid
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F(InteractionManagerTest, InteractionManagerTest_GetDragTargetPid, TestSize.Level1)
-{
-    CALL_TEST_DEBUG;
-    int32_t pid = InteractionManager::GetInstance()->GetDragTargetPid();
-    ASSERT_TRUE(pid >= -1);
-}
+//  /**
+// *  @tc.name: InteractionManagerTest_GetDragTargetPid
+//  * @tc.desc: Get Drag Target Pid
+//  * @tc.type: FUNC
+//  * @tc.require:
+//  */
+// HWTEST_F(InteractionManagerTest, InteractionManagerTest_GetDragTargetPid, TestSize.Level1)
+// {
+//     CALL_TEST_DEBUG;
+//     int32_t pid = InteractionManager::GetInstance()->GetDragTargetPid();
+//     ASSERT_TRUE(pid >= -1);
+// }
 
 } // namespace DeviceStatus
 } // namespace Msdp
