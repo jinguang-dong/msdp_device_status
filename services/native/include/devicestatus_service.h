198592bdb04bd8f9cb494186b84b8ad7188a823d
blueyouth <bailiang18@huawei.com>
2024-09-07 01:39:06 +0000

    remove redundant code
    Signed-off-by: blueyouth <bailiang18@huawei.com>
    Change-Id: Ie3b85fa9550ee577c55911b13ec97fdba1885d6f

--- services/native/include/devicestatus_service.h
+++ services/native/include/devicestatus_service.h
@@ -43,8 +43,7 @@ namespace DeviceStatus {
 enum class ServiceRunningState {STATE_NOT_START, STATE_RUNNING, STATE_EXIT};
 class DeviceStatusService final : public IContext,
                                   public StreamServer,
-                                  public SystemAbility,
-                                  public DeviceStatusSrvStub {
+                                  public SystemAbility {
     DECLARE_SYSTEM_ABILITY(DeviceStatusService)
     DECLARE_DELAYED_SP_SINGLETON(DeviceStatusService);
 public:
@@ -64,57 +63,13 @@ public:
     IDSoftbusAdapter& GetDSoftbus() override;
 
     void OnAddSystemAbility(int32_t saId, const std::string &deviceId) override;
-    void Subscribe(Type type, ActivityEvent event, ReportLatencyNs latency,
-        sptr<IRemoteDevStaCallback> callback) override;
-    void Unsubscribe(Type type, ActivityEvent event, sptr<IRemoteDevStaCallback> callback) override;
-    Data GetCache(const Type &type) override;
+    Data GetCache(const Type &type);
     bool IsServiceReady() const;
-    int32_t Dump(int32_t fd, const std::vector<std::u16string> &args) override;
-#ifdef MSDP_HIVIEWDFX_HISYSEVENT_ENABLE
-    void ReportSensorSysEvent(int32_t type, bool enable);
-#endif
-    int32_t RegisterCoordinationListener(bool isCompatible = false) override;
-    int32_t UnregisterCoordinationListener(bool isCompatible = false) override;
-    int32_t PrepareCoordination(int32_t userData, bool isCompatible = false) override;
-    int32_t UnprepareCoordination(int32_t userData, bool isCompatible = false) override;
-    int32_t ActivateCoordination(int32_t userData, const std::string &remoteNetworkId, int32_t startDeviceId,
-        bool isCompatible = false) override;
-    int32_t DeactivateCoordination(int32_t userData, bool isUnchained, bool isCompatible = false) override;
-    int32_t GetCoordinationState(int32_t userData, const std::string &networkId,
-        bool isCompatible = false) override;
-    int32_t GetCoordinationState(const std::string &udId, bool &state) override;
-    int32_t StartDrag(const DragData &dragData) override;
-    int32_t StopDrag(const DragDropResult &dropResult) override;
-    int32_t UpdateDragStyle(DragCursorStyle style) override;
-    int32_t GetDragTargetPid() override;
-    int32_t GetUdKey(std::string &udKey) override;
-    int32_t AddDraglistener() override;
-    int32_t RemoveDraglistener() override;
-    int32_t AddSubscriptListener() override;
-    int32_t RemoveSubscriptListener() override;
-    int32_t SetDragWindowVisible(bool visible, bool isForce = false) override;
-    int32_t EnterTextEditorArea(bool enable) override;
-    int32_t GetShadowOffset(ShadowOffset &shadowOffset) override;
-    int32_t UpdateShadowPic(const ShadowInfo &shadowInfo) override;
-    int32_t GetDragData(DragData &dragData) override;
-    int32_t GetDragState(DragState &dragState) override;
-    int32_t GetDragAction(DragAction &dragAction) override;
-    int32_t GetExtraInfo(std::string &extraInfo) override;
-    int32_t AllocSocketFd(const std::string &programName, int32_t moduleType,
-    int32_t &toReturnClientFd, int32_t &tokenType) override;
+    int32_t Dump(int32_t fd, const std::vector<std::u16string> &args);
     void OnConnected(SessionPtr s) override;
     void OnDisconnected(SessionPtr s) override;
     int32_t AddEpoll(EpollEventType type, int32_t fd) override;
     int32_t DelEpoll(EpollEventType type, int32_t fd);
-    bool IsRunning() const override;
-    int32_t AddHotAreaListener() override;
-    int32_t RemoveHotAreaListener() override;
-    int32_t UpdatePreviewStyle(const PreviewStyle &previewStyle) override;
-    int32_t UpdatePreviewStyleWithAnimation(const PreviewStyle &previewStyle,
-        const PreviewAnimation &animation) override;
-    int32_t GetDragSummary(std::map<std::string, int64_t> &summarys) override;
-    int32_t AddPrivilege() override;
-    int32_t EraseMouseIcon() override;
 
 private:
     bool Init();
@@ -132,22 +87,6 @@ private:
     void DisableDevMgr();
     void EnableDSoftbus();
     void EnableDDM();
-
-#ifdef OHOS_BUILD_ENABLE_COORDINATION
-#ifndef OHOS_BUILD_ENABLE_INTENTION_FRAMEWORK
-    int32_t OnAddHotAreaListener(int32_t pid);
-    int32_t OnRemoveHotAreaListener(int32_t pid);
-    int32_t OnRegisterCoordinationListener(int32_t pid);
-    int32_t OnUnregisterCoordinationListener(int32_t pid);
-    int32_t OnPrepareCoordination(int32_t pid, int32_t userData);
-    int32_t OnUnprepareCoordination(int32_t pid, int32_t userData);
-    int32_t OnActivateCoordination(int32_t pid, int32_t userData, const std::string &remoteNetworkId,
-        int32_t startDeviceId);
-    int32_t OnDeactivateCoordination(int32_t pid, int32_t userData, bool isUnchained);
-    int32_t OnGetCoordinationState(int32_t pid, int32_t userData, const std::string &networkId);
-    int32_t OnGetCoordinationStateSync(const std::string &udId, bool &state);
-#endif // OHOS_BUILD_ENABLE_INTENTION_FRAMEWORK
-#endif // OHOS_BUILD_ENABLE_COORDINATION
 
 private:
     std::atomic<ServiceRunningState> state_ { ServiceRunningState::STATE_NOT_START };
