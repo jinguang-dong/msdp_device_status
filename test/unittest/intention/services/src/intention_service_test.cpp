e839ac052a9f4c0f227366abb1a7264d6a2a360c
yang123 <yangyanling13@huawei.com>
2024-10-11 02:02:43 +0000

    update test/unittest/intention/services/src/intention_service_test.cpp.
    Signed-off-by: yang123 <yangyanling13@huawei.com>

--- test/unittest/intention/services/src/intention_service_test.cpp
+++ test/unittest/intention/services/src/intention_service_test.cpp
@@ -16,7 +16,6 @@
 #define BUFF_SIZE 100
 #include "intention_service_test.h"
 
-#include "ddm_adapter.h"
 #include "drag_data_manager.h"
 #include "drag_params.h"
 #include "drag_server.h"
@@ -89,7 +88,6 @@ ContextService* ContextService::GetInstance()
 
 ContextService::ContextService()
 {
-    ddm_ = std::make_unique<DDMAdapter>();
     input_ = std::make_unique<InputAdapter>();
     pluginMgr_ = std::make_unique<PluginManager>(this);
     dsoftbus_ = std::make_unique<DSoftbusAdapter>();
@@ -118,11 +116,6 @@ IDragManager& ContextService::GetDragManager()
 ISocketSessionManager& ContextService::GetSocketSessionManager()
 {
     return socketSessionMgr_;
-}
-
-IDDMAdapter& ContextService::GetDDM()
-{
-    return *ddm_;
 }
 
 IPluginManager& ContextService::GetPluginManager()
