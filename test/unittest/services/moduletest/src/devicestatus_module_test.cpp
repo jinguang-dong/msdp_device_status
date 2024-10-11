c877666bfdc2d578828da391968e4ce43cad4bf1
yang123 <yangyanling13@huawei.com>
2024-10-11 03:29:48 +0000

    unittest1
    Signed-off-by: yang123 <yangyanling13@huawei.com>

---
+++ test/unittest/services/moduletest/src/devicestatus_module_test.cpp
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "devicestatus_module_test.h"
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include <gtest/gtest.h>
+#include <if_system_ability_manager.h>
+#include <ipc_skeleton.h>
+#include <string_ex.h>
+
+#include "fi_log.h"
+#include "stationary_manager.h"
+
+#undef LOG_TAG
+#define LOG_TAG "DeviceStatusModuleTest"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+using namespace testing::ext;
+
+Type DeviceStatusModuleTest::g_moduleTest = Type::TYPE_INVALID;
+
+void DeviceStatusModuleTest::DeviceStatusModuleTestCallback::OnDeviceStatusChanged(const Data &devicestatusData)
+{
+    GTEST_LOG_(INFO) << "DeviceStatusModuleTestCallback type: " << devicestatusData.type;
+    GTEST_LOG_(INFO) << "DeviceStatusModuleTestCallback value: " << devicestatusData.value;
+    EXPECT_EQ(true, devicestatusData.type == g_moduleTest &&
+        (devicestatusData.value >= OnChangedValue::VALUE_INVALID &&
+        devicestatusData.value <= OnChangedValue::VALUE_EXIT)) <<
+        "DeviceStatusModuleTestCallback failed";
+}
+
+namespace {
+/**
+ * @tc.name: DeviceStatusCallbackTest
+ * @tc.desc: test devicestatus callback in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusModuleTest, DeviceStatusCallbackTest, TestSize.Level0)
+{
+    g_moduleTest = Type::TYPE_ABSOLUTE_STILL;
+    Type type = g_moduleTest;
+    auto stationaryManager = StationaryManager::GetInstance();
+    sptr<IRemoteDevStaCallback> cb = new (std::nothrow) DeviceStatusModuleTestCallback();
+    EXPECT_FALSE(cb == nullptr);
+    GTEST_LOG_(INFO) << "DeviceStatusCallbackTest start register";
+    stationaryManager->SubscribeCallback(type, ActivityEvent::ENTER_EXIT, ReportLatencyNs::LONG, cb);
+    GTEST_LOG_(INFO) << "DeviceStatusCallbackTest cancel register";
+    stationaryManager->UnsubscribeCallback(type, ActivityEvent::ENTER_EXIT, cb);
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusModuleTest, GetDeviceStatusDataTest001, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_moduleTest = Type::TYPE_HORIZONTAL_POSITION;
+    Type type = g_moduleTest;
+    auto stationaryManager = StationaryManager::GetInstance();
+    Data data = stationaryManager->GetDeviceStatusData(type);
+    GTEST_LOG_(INFO) << "type: " << data.type;
+    GTEST_LOG_(INFO) << "value: " << data.value;
+    EXPECT_EQ(true, data.type == g_moduleTest &&
+        data.value >= OnChangedValue::VALUE_INVALID &&
+        data.value <= OnChangedValue::VALUE_EXIT) << "GetDeviceStatusData failed";
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusModuleTest, GetDeviceStatusDataTest002, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_moduleTest = Type::TYPE_ABSOLUTE_STILL;
+    Type type = g_moduleTest;
+    auto stationaryManager = StationaryManager::GetInstance();
+    Data data = stationaryManager->GetDeviceStatusData(type);
+    GTEST_LOG_(INFO) << "type: " << data.type;
+    GTEST_LOG_(INFO) << "value: " << data.value;
+    EXPECT_EQ(true, data.type == g_moduleTest &&
+        data.value >= OnChangedValue::VALUE_INVALID &&
+        data.value <= OnChangedValue::VALUE_EXIT) << "GetDeviceStatusData failed";
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusModuleTest, GetDeviceStatusDataTest003, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_moduleTest = Type::TYPE_VERTICAL_POSITION;
+    Type type = g_moduleTest;
+    auto stationaryManager = StationaryManager::GetInstance();
+    Data data = stationaryManager->GetDeviceStatusData(type);
+    GTEST_LOG_(INFO) << "type: " << data.type;
+    GTEST_LOG_(INFO) << "value: " << data.value;
+    EXPECT_EQ(true, data.type == g_moduleTest &&
+        data.value >= OnChangedValue::VALUE_INVALID &&
+        data.value <= OnChangedValue::VALUE_EXIT) << "GetDeviceStatusData failed";
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusModuleTest, GetDeviceStatusDataTest004, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_moduleTest = Type::TYPE_STILL;
+    Type type = g_moduleTest;
+    Data data = StationaryManager::GetInstance()->GetDeviceStatusData(type);
+    GTEST_LOG_(INFO) << "type: " << data.type;
+    GTEST_LOG_(INFO) << "value: " << data.value;
+    EXPECT_EQ(true, data.type == g_moduleTest &&
+        data.value >= OnChangedValue::VALUE_INVALID &&
+        data.value <= OnChangedValue::VALUE_EXIT) << "GetDeviceStatusData failed";
+}
+} // namespace
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
---
+++ test/unittest/services/unittest/src/devicestatus_service_test.cpp
@@ -0,0 +1,364 @@
+/*
+ * Copyright (c) 2022-2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "devicestatus_service_test.h"
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include <gtest/gtest.h>
+#include <if_system_ability_manager.h>
+#include <ipc_skeleton.h>
+#include <string_ex.h>
+
+#include "fi_log.h"
+#include "stationary_manager.h"
+
+#undef LOG_TAG
+#define LOG_TAG "DeviceStatusServiceTest"
+
+namespace OHOS {
+namespace Msdp {
+namespace DeviceStatus {
+using namespace testing::ext;
+namespace {
+constexpr int32_t SLEEP_TIME { 2000 };
+Type g_type { Type::TYPE_INVALID };
+auto g_client = StationaryManager::GetInstance();
+} // namespace
+
+void DeviceStatusServiceTest::SetUpTestCase()
+{
+    devCallback_ = new (std::nothrow) DeviceStatusServiceTestCallback();
+}
+
+void DeviceStatusServiceTest::TearDownTestCase() {}
+
+void DeviceStatusServiceTest::SetUp() {}
+
+void DeviceStatusServiceTest::TearDown() {}
+
+void DeviceStatusServiceTest::DeviceStatusServiceTestCallback::OnDeviceStatusChanged(const Data& devicestatusData)
+{
+    GTEST_LOG_(INFO) << "DeviceStatusServiceTestCallback type: " << devicestatusData.type;
+    GTEST_LOG_(INFO) << "DeviceStatusServiceTestCallback value: " << devicestatusData.value;
+    EXPECT_TRUE(devicestatusData.type == g_type && (devicestatusData.value >= OnChangedValue::VALUE_INVALID &&
+        devicestatusData.value <= OnChangedValue::VALUE_EXIT)) << "DeviceStatusServiceTestCallback failed";
+}
+
+namespace {
+/**
+ * @tc.name: DeviceStatusCallbackTest
+ * @tc.desc: test devicestatus callback in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, DeviceStatusCallbackTest001, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_ABSOLUTE_STILL;
+    EXPECT_FALSE(devCallback_ == nullptr);
+    GTEST_LOG_(INFO) << "Start register";
+    g_client->SubscribeCallback(g_type, ActivityEvent::ENTER_EXIT, ReportLatencyNs::LONG, devCallback_);
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest002, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_ABSOLUTE_STILL;
+    Data data = g_client->GetDeviceStatusData(g_type);
+    GTEST_LOG_(INFO) << "type: " << data.type;
+    GTEST_LOG_(INFO) << "value: " << data.value;
+    EXPECT_TRUE(data.type == Type::TYPE_ABSOLUTE_STILL &&
+        (data.value >= OnChangedValue::VALUE_INVALID && data.value <= OnChangedValue::VALUE_EXIT));
+    GTEST_LOG_(INFO) << "GetDeviceStatusData failed";
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest003, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_ABSOLUTE_STILL;
+    EXPECT_FALSE(devCallback_ == nullptr);
+    GTEST_LOG_(INFO) << "Cancel register";
+    g_client->UnsubscribeCallback(g_type, ActivityEvent::ENTER_EXIT, devCallback_);
+}
+
+/**
+ * @tc.name: DeviceStatusCallbackTest
+ * @tc.desc: test devicestatus callback in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, DeviceStatusCallbackTest004, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_CAR_BLUETOOTH;
+    EXPECT_FALSE(devCallback_ == nullptr);
+    GTEST_LOG_(INFO) << "Start register";
+    g_client->SubscribeCallback(g_type, ActivityEvent::ENTER_EXIT, ReportLatencyNs::LONG, devCallback_);
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest005, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_CAR_BLUETOOTH;
+    Data data = g_client->GetDeviceStatusData(g_type);
+    GTEST_LOG_(INFO) << "type: " << data.type;
+    GTEST_LOG_(INFO) << "value: " << data.value;
+    EXPECT_TRUE(data.type == Type::TYPE_CAR_BLUETOOTH &&
+        (data.value <= OnChangedValue::VALUE_EXIT && data.value >= OnChangedValue::VALUE_INVALID));
+    GTEST_LOG_(INFO) << "GetDeviceStatusData failed";
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest006, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_CAR_BLUETOOTH;
+    EXPECT_FALSE(devCallback_ == nullptr);
+    GTEST_LOG_(INFO) << "Cancel register";
+    g_client->UnsubscribeCallback(g_type, ActivityEvent::ENTER_EXIT, devCallback_);
+}
+
+/**
+ * @tc.name: DeviceStatusCallbackTest
+ * @tc.desc: test devicestatus callback in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, DeviceStatusCallbackTest007, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_HORIZONTAL_POSITION;
+    EXPECT_FALSE(devCallback_ == nullptr);
+    GTEST_LOG_(INFO) << "Start register";
+    g_client->SubscribeCallback(g_type, ActivityEvent::ENTER_EXIT, ReportLatencyNs::LONG, devCallback_);
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest008, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_HORIZONTAL_POSITION;
+    Data data = g_client->GetDeviceStatusData(g_type);
+    GTEST_LOG_(INFO) << "type: " << data.type;
+    GTEST_LOG_(INFO) << "value: " << data.value;
+    EXPECT_TRUE(data.type == Type::TYPE_HORIZONTAL_POSITION &&
+        (data.value >= OnChangedValue::VALUE_INVALID && data.value <= OnChangedValue::VALUE_EXIT));
+    GTEST_LOG_(INFO) << "GetDeviceStatusData failed";
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest009, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_HORIZONTAL_POSITION;
+    EXPECT_FALSE(devCallback_ == nullptr);
+    GTEST_LOG_(INFO) << "Cancel register";
+    g_client->UnsubscribeCallback(g_type, ActivityEvent::ENTER_EXIT, devCallback_);
+}
+
+/**
+ * @tc.name: DeviceStatusCallbackTest
+ * @tc.desc: test devicestatus callback in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, DeviceStatusCallbackTest010, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_RELATIVE_STILL;
+    EXPECT_FALSE(devCallback_ == nullptr);
+    GTEST_LOG_(INFO) << "Start register";
+    g_client->SubscribeCallback(g_type, ActivityEvent::ENTER_EXIT, ReportLatencyNs::LONG, devCallback_);
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest011, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_RELATIVE_STILL;
+    Data data = g_client->GetDeviceStatusData(g_type);
+    GTEST_LOG_(INFO) << "type: " << data.type;
+    GTEST_LOG_(INFO) << "value: " << data.value;
+    EXPECT_TRUE(data.type == Type::TYPE_RELATIVE_STILL &&
+        (data.value <= OnChangedValue::VALUE_EXIT && data.value >= OnChangedValue::VALUE_INVALID));
+    GTEST_LOG_(INFO) << "GetDeviceStatusData failed";
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest012, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_RELATIVE_STILL;
+    EXPECT_FALSE(devCallback_ == nullptr);
+    GTEST_LOG_(INFO) << "Cancel register";
+    g_client->UnsubscribeCallback(g_type, ActivityEvent::ENTER_EXIT, devCallback_);
+}
+
+/**
+ * @tc.name: DeviceStatusCallbackTest
+ * @tc.desc: test devicestatus callback in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, DeviceStatusCallbackTest013, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_STILL;
+    EXPECT_FALSE(devCallback_ == nullptr);
+    GTEST_LOG_(INFO) << "Start register";
+    g_client->SubscribeCallback(g_type, ActivityEvent::ENTER_EXIT, ReportLatencyNs::LONG, devCallback_);
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest014, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_STILL;
+    Data data = g_client->GetDeviceStatusData(g_type);
+    GTEST_LOG_(INFO) << "type: " << data.type;
+    GTEST_LOG_(INFO) << "value: " << data.value;
+    EXPECT_TRUE(data.type == Type::TYPE_STILL &&
+        (data.value <= OnChangedValue::VALUE_EXIT && data.value >= OnChangedValue::VALUE_INVALID));
+    GTEST_LOG_(INFO) << "GetDeviceStatusData failed";
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest015, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_STILL;
+    EXPECT_FALSE(devCallback_ == nullptr);
+    GTEST_LOG_(INFO) << "Cancel register";
+    g_client->UnsubscribeCallback(g_type, ActivityEvent::ENTER_EXIT, devCallback_);
+}
+
+/**
+ * @tc.name: DeviceStatusCallbackTest
+ * @tc.desc: test devicestatus callback in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, DeviceStatusCallbackTest016, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_VERTICAL_POSITION;
+    EXPECT_FALSE(devCallback_ == nullptr);
+    GTEST_LOG_(INFO) << "Start register";
+    g_client->SubscribeCallback(g_type, ActivityEvent::ENTER_EXIT, ReportLatencyNs::LONG, devCallback_);
+    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest017, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_VERTICAL_POSITION;
+    Data data = g_client->GetDeviceStatusData(g_type);
+    GTEST_LOG_(INFO) << "type: " << data.type;
+    GTEST_LOG_(INFO) << "value: " << data.value;
+    EXPECT_TRUE(data.type == Type::TYPE_VERTICAL_POSITION &&
+        (data.value >= OnChangedValue::VALUE_INVALID && data.value <= OnChangedValue::VALUE_EXIT));
+    GTEST_LOG_(INFO) << "GetDeviceStatusData failed";
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest018, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_VERTICAL_POSITION;
+    EXPECT_FALSE(devCallback_ == nullptr);
+    GTEST_LOG_(INFO) << "Cancel register";
+    g_client->UnsubscribeCallback(g_type, ActivityEvent::ENTER_EXIT, devCallback_);
+}
+
+/**
+ * @tc.name: GetDeviceStatusDataTest
+ * @tc.desc: test get devicestatus data in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F(DeviceStatusServiceTest, GetDeviceStatusDataTest019, TestSize.Level0)
+{
+    CALL_TEST_DEBUG;
+    g_type = Type::TYPE_VERTICAL_POSITION;
+    Data data = g_client->GetDeviceStatusData(g_type);
+    GTEST_LOG_(INFO) << "type: " << data.type;
+    GTEST_LOG_(INFO) << "value: " << data.value;
+    EXPECT_TRUE(data.type == Type::TYPE_VERTICAL_POSITION &&
+        (data.value >= OnChangedValue::VALUE_INVALID && data.value <= OnChangedValue::VALUE_EXIT));
+    GTEST_LOG_(INFO) << "GetDeviceStatusData failed";
+    Data invalidData;
+    invalidData.type = Type::TYPE_INVALID;
+    invalidData.value = OnChangedValue::VALUE_INVALID;
+    invalidData.status = Status::STATUS_INVALID;
+    invalidData.movement = 0.0f;
+    EXPECT_TRUE(data != invalidData);
+}
+} // namespace
+} // namespace DeviceStatus
+} // namespace Msdp
+} // namespace OHOS
